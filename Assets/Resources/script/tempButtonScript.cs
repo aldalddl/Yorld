ee3491945451d3b44db68be31e8fe9f7d7c36ad6
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-07-23 02:21:07 +0900

    test 1

---
+++ Assets/Resources/script/tempButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempButtonScript : MonoBehaviour
+{
+    public Transform tree;
+    public Vector3 plane;
+    public Quaternion quater;
+    public GameObject marker;
+
+    public void OnClickNewTree()
+    {
+        marker = GameObject.FindGameObjectWithTag("imgTarget");
+        plane = GameObject.FindGameObjectWithTag("plane").transform.position;
+        if (marker.transform.position.x != 0) //화면에 마커가 포착됐는지 검사해야 하는데 뭔가 잘못 됨...
+        {
+            plane.y = plane.y + 0.2f;
+            quater = GameObject.FindGameObjectWithTag("plane").transform.rotation;
+            Instantiate(tree, plane, quater).SetParent(marker.transform);
+        }
+    }
+}
